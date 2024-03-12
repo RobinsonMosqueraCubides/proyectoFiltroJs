318644862d734ce80721c400da5a743b9a47015b
RobinsonMosqueraCubides <152357795+RobinsonMosqueraCubides@users.noreply.github.com>
2024-03-12 08:27:50 -0500

    fix: :bug: se corrige el bug de activos
    Se corrige el bug al darle click a activo

--- js/main.js
+++ js/main.js
@@ -3,22 +3,23 @@ import './crearNuevoActivo.js';
 import './buscar.js';
 import './agregar.js'
 import './editar.js';
-function limpador() {
+
+function limpador() {//limpia la pantalla para agregar el web component
     let main = document.getElementById('principal')
     let titulo = document.querySelector('.title')
     titulo.innerHTML='';
     main.innerHTML='';
 }
-function reconocer(x) {
+function reconocer(x) {//obtiene el nombre de la clase y del id de donde se le da click
     let Elements = document.querySelectorAll(x);
     Elements.forEach(function(event){
         event.addEventListener('click', function(){
             let className = event.className;
             let idName =event.id
             if (className == 'activos' && idName =='agregar') {
-                webComponentAgregarActivo();
-            }
-            switch (idName) {
+                callDuck(className,'crear');
+            }else{
+            switch (idName) {//dependiendo del id se crea llama la funcion para crear el web component
                 case 'eliminar':
                     callDuck(className, 'eliminar');
                     break;
@@ -34,6 +35,7 @@ function reconocer(x) {
                 default:
                     break;
             }
+            }
             
         });
     }); 
@@ -46,7 +48,7 @@ reconocer(".tipoPersona");
 reconocer(".tipoMovActivo");
 reconocer(".tipos");
 reconocer(".asignacion");
-function callDuck(name, nameComponent) {
+function callDuck(name, nameComponent) { //crea el web component dependiendo de lo que le envie la funcion reconocer
     limpador();
     let creator = document.querySelector('.title')
     let base = `<${nameComponent}-activo name = ${name}></${nameComponent}-activo>`
@@ -55,13 +57,5 @@ function callDuck(name, nameComponent) {
     tdic.innerHTML = base;
     creator.appendChild(tdic);
 }
-function webComponentAgregarActivo() {
-    limpador();
-    let creator = document.querySelector('.title')
-    let base = `<crear-activo></crear-activo>`
-    let tdic = document.createElement('div');
-    tdic.classList.add('informacion')
-    tdic.innerHTML = base;
-    creator.appendChild(tdic);
-}
+
 
